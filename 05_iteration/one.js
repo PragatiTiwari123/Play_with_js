62029ca14b1dea35afc022a974a0c75aa5bba9f3
PragatiTiwari123 <pragatip963@gmail.com>
2023-12-30 12:53:38 +0530

    Add iteration's first file

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -13,9 +13,40 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i < 10; i++) {
-    console.log(`Outer loop : ${i}`);
-    for (let j = 0; j < 10; j++) {
-        console.log(`Inner loop : ${j} and Inner loop : ${i}`); 
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop : ${i}`);
+//     console.log(`Table of : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop : ${j} and Inner loop : ${i}`); 
+//         console.log(i + ' * '+j+' = '+(i*j));
+//     }
+// }
+
+let myArray = ["Flash" , "batman" , "superman"];
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+// for (let index = 0; index <= myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element); // Flash  batman  superman    undefined
+// }
+
+// break continue
+
+for (let index = 1; index < 20; index++) {
+    if(index==5){
+        console.log("Detected 5");
+        break;
     }
+    console.log(`Value of i is ${index}`);
+}
+
+for (let index = 1; index < 20; index++) {
+    if(index==5){
+        console.log("Detected 5");
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
 }
