42d4b8febb61e928547f0a72fad0bdee69e2f6e5
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-06 15:39:24 +0530

    Third DOM Project : update feature (completed)

--- 07_JS_PROJECTS/dom3.js
+++ 07_JS_PROJECTS/dom3.js
@@ -1,13 +1,24 @@
 var istatus=document.querySelector("h5")
 
 var addFriend =  document.querySelector('#add')
-var removeFriend = document.querySelector('#remove')
+var flag = 0
 
 addFriend.addEventListener("click",function () {
-    istatus.innerHTML = "Friends"
-    istatus.style.color = "green"
+    if(flag === 0){
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+        addFriend.innerHTML = "Remove"
+        addFriend.style.color ="#111"
+        addFriend.style.backgroundColor = "#dadada"
+        flag=1
+    }
+    else{
+        istatus.innerHTML = "Stranger"
+        istatus.style.color = "red"
+        addFriend.innerHTML = "Add Friend"
+        addFriend.style.color ="#fff"
+        addFriend.style.backgroundColor = "cadetblue"
+        flag=0
+    }
 })
-removeFriend.addEventListener("click",function () {
-    istatus.innerHTML = "Stranger"
-    istatus.style.color = "red"
-})
+
