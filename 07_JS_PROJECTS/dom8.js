8e3f40b4d6c9ec94ca4e18d7b74f7c3cb67dad97
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-09 14:15:37 +0530

    eight project : update code

--- 07_JS_PROJECTS/dom8.js
+++ 07_JS_PROJECTS/dom8.js
@@ -9,23 +9,41 @@ var clutter = ""
 arr.forEach(function(elem,i){
     clutter += `<div class="story">
     <img id="${i}" src="${elem.dp}" alt="Profile">
-</div>`
+    </div>`
 })
 
 
 stories.innerHTML = clutter;
 
-stories.addEventListener("click",function(e){
-    // console.log(e); //prints pointer
-    // console.log(e.target); // print the img tag #targeted element
-    // console.log(e.target.id);   // id 0,1,2,3 index
-    // console.log(arr[e.target.id]); // arr[0],arr[1]..
-    // console.log(arr[e.target.id].story);  // arr[0].story , arr[1].story , ..
-    // console.log(arr[e.target.id].story);
-    document.querySelector(".full-screen").style.display = 'block';
-    // document.querySelector(".full-screen").style.backgroundColor = 'blue'; 
-    document.querySelector(".full-screen").style.backgroundImage = `url("${arr[e.target.id].story}")`;
-    setTimeout(function () {
-        document.querySelector(".full-screen").style.display = 'none';
-    },3000)
+
+arr.forEach(function(elem,i){
+    // console.log(stories.children[i]);
+    stories.children[i].addEventListener("click",function(e){
+        // console.log(e); //prints pointer
+        // console.log(e.target); // print the img tag #targeted element
+        // console.log(e.target.id);   // id 0,1,2,3 index
+        // console.log(arr[e.target.id]); // arr[0],arr[1]..
+        // console.log(arr[e.target.id].story);  // arr[0].story , arr[1].story , ..
+        // console.log(arr[e.target.id].story);
+        document.querySelector(".full-screen").style.display = 'block';
+        // document.querySelector(".full-screen").style.backgroundColor = 'blue'; 
+        document.querySelector(".full-screen").style.backgroundImage = `url("${arr[e.target.id].story}")`;
+        setTimeout(function () {
+            document.querySelector(".full-screen").style.display = 'none';
+        },3000)
+    })
 })
+// stories.addEventListener("click",function(e){
+//     // console.log(e); //prints pointer
+//     // console.log(e.target); // print the img tag #targeted element
+//     // console.log(e.target.id);   // id 0,1,2,3 index
+//     // console.log(arr[e.target.id]); // arr[0],arr[1]..
+//     // console.log(arr[e.target.id].story);  // arr[0].story , arr[1].story , ..
+//     // console.log(arr[e.target.id].story);
+//     document.querySelector(".full-screen").style.display = 'block';
+//     // document.querySelector(".full-screen").style.backgroundColor = 'blue'; 
+//     document.querySelector(".full-screen").style.backgroundImage = `url("${arr[e.target.id].story}")`;
+//     setTimeout(function () {
+//         document.querySelector(".full-screen").style.display = 'none';
+//     },3000)
+// })
