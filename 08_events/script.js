b10faf8a1e68b579d6161356c74991901739c3b8
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-12 23:53:28 +0530

    Solved Corner Case : Save Memory

--- 08_events/script.js
+++ 08_events/script.js
@@ -10,15 +10,38 @@ function Randomhexcode(){
 //  let RandomColor = Randomhexcode()
 //  console.log(RandomColor);
 
+// let intervalId ;
+// let StartColorChanging = function(){
+//      let changeColor =function(){
+//         document.body.style.backgroundColor = Randomhexcode();
+//      }
+//     intervalId = setInterval(changeColor,1000);
+// }
+// let StopColorChanging = function(){
+//     clearInterval(intervalId);
+// }
+// document.querySelector("#start").addEventListener('click',function(){
+//     StartColorChanging();
+// })
+// document.querySelector("#stop").addEventListener('click',function(){
+//     StopColorChanging();
+// })
+
+// FOr an optimized Approach 
+// Corner Case : Memory is wasted when we stop i.e. not changing code we want it to free from the memory and we want next time we create it again
+
 let intervalId ;
 let StartColorChanging = function(){
      let changeColor =function(){
-        document.body.style.backgroundColor = Randomhexcode();
+        if(!intervalId){ // if null
+            document.body.style.backgroundColor = Randomhexcode();
+        }
      }
     intervalId = setInterval(changeColor,1000);
 }
 let StopColorChanging = function(){
     clearInterval(intervalId);
+    intervalId = null;
 }
 document.querySelector("#start").addEventListener('click',function(){
     StartColorChanging();
