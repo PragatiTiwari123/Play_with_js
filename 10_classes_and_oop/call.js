e29f5729ade40924d7cdf150c3751e2459f26bb7
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-19 20:52:35 +0530

    this call

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,11 @@
+function setUserName(username){
+    this.username=username
+    console.log('called');
+}
+function createUser(username, email,password){
+    setUserName.call(this,username)
+    this.email=email
+    this.password=password
+}
+const chaii= new createUser('chai','chai@pgmail.com','123')
+console.log(chaii);
