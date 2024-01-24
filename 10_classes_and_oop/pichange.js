51d9f0b17f3d400e25acdca376c00814c38a7a7e
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-24 17:49:35 +0530

    more about objects

--- 10_classes_and_oop/pichange.js
+++ 10_classes_and_oop/pichange.js
@@ -4,9 +4,31 @@ const descripter = Object.getOwnPropertyDescriptor(Math,'PI');
 // console.log(Math.PI);
 
 descripter.writable=true;
-descripter.enumerable=true;
+// descripter.enumerable=true;
 // descripter.configurable=true;
 descripter.value = 5
-console.log(descripter);
+// console.log(descripter);
 
-console.log(descripter.value);
+// console.log(descripter.value);
+
+// const myObj = Object.create(obj)
+const chai ={
+    name: 'ginger-chai',
+    price:250,
+    isAvailable:true,
+    orderChai:function(){
+        console.log("Chai nahi bani");
+    }
+}
+// console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai,'name'))
+Object.defineProperty(chai,'name',{
+    // writable:false,
+    enumerable:false,
+})
+// console.log(Object.getOwnPropertyDescriptor(chai,'name'))
+
+for (const [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function')
+    console.log(`${key} : ${value}`);
+}
